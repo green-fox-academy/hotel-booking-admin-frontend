cb071dac15ff4cbf07a56af3241854f9e3835bf9
Zsolt Makrai <mecrai@gmail.com>
2017-06-08 15:38:09 +0200

    refactor(test): test refactored

--- admin-frontend/src/app/app.component.spec.ts
+++ admin-frontend/src/app/app.component.spec.ts
@@ -19,10 +19,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Hotel Booking Admin'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Hotel Booking Admin');
   }));
 
   it('should render title in a h1 tag', async(() => {
