c8282cba6a1046bcac720169e8d5dc12aa1160f4
Fanni Fulmer <fannifulmer@gmail.com>
2017-06-09 11:45:05 +0200

    refactor(json): deleted test and modified port

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+// import { provideRoutes } from '@angular/router';
 
 import { appRouting } from './app.routing';
 import { AppComponent } from './app.component';
