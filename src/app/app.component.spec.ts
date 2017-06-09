8be1ac4ec784436595212f8c5417dccca5c153d3
Zsolt Makrai <mecrai@gmail.com>
2017-06-09 12:02:14 +0200

    test(app): router test added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+
+// beforeEach(async(() => {
+//   TestBed.configureTestingModule({
+//     imports: [
+//       RouterTestingModule
+//     ],
+//     declarations: [
+//       YourComponent
+//     ]
+//   }).compileComponents();
+// }));
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+        imports: [
+            RouterTestingModule
+        ],
       declarations: [
         AppComponent
       ],
