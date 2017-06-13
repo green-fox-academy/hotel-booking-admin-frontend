d3db224262a6a1b0bd2d15723dc865607fcfbd6d
Zsolt Makrai <mecrai@gmail.com>
2017-06-13 19:00:13 +0200

    server test

--- server.js
+++ server.js
@@ -3,15 +3,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mockServer = require('./mock-server.js');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-if (process.env.APP_ENV === 'MOCK') {
-    mockServer();
-} else {
-    mockServer();
-};
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// if (process.env.APP_ENV === 'MOCK') {
+//     mockServer();
+// } else {
+//     mockServer();
+// };
 
 app.listen(process.env.PORT || 8080, () => console.log('server is running'));
