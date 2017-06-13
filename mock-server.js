c3c2d3ae102a220879b5cc404bf71293e10c6805
Zsolt Makrai <mecrai@gmail.com>
2017-06-13 17:12:53 +0200

    feat(mock server): server file created

--- mock-server.js
+++ mock-server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(express.static(__dirname + '/dist'));
+
 const user = {
     username: 'Test Admin',
     password: '1234'
