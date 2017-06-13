2c3594b6094fb27e8bc8a3dc32c2b44c43a4c337
Zsolt Makrai <mecrai@gmail.com>
2017-06-13 19:34:15 +0200

    server test

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if (process.env.APP_ENV === 'MOCK') {
         res.sendFile(path.join(__dirname + '/dist/index.html'));
     });
 
-    app.post('/api/login/', (req, res) => {
+    app.get('/api/login/', (req, res) => {
         const username = req.body.username;
         const password = req.body.password;
         if (username === user.username && password === user.password) {
@@ -46,35 +46,6 @@ if (process.env.APP_ENV === 'MOCK') {
     });
 } else {
     // mockServer();
-    const user = {
-        username: 'Test Admin',
-        password: '1234'
-    };
-    const validResponse = {
-        status: 'ok',
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3RBZG1pbiIsImFkbWluIjp0cnVlfQ.nhC1EDI5xLGM4yZL2VMZyvHcbcWiXM2RVS7Y8Pt0Zuk'
-    };
-
-    const invalidResponse = {
-        status: 'error',
-        message: 'Mismatched username and password'
-    };
-
-    app.use(bodyParser.json());
-
-    app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/dist/index.html'));
-    });
-
-    app.post('/api/login/', (req, res) => {
-        const username = req.body.username;
-        const password = req.body.password;
-        if (username === user.username && password === user.password) {
-            res.send(validResponse);
-        } else {
-            res.send(invalidResponse);
-        }
-    });
 };
 
 app.listen(process.env.PORT || 8080, () => console.log('server is running'));
