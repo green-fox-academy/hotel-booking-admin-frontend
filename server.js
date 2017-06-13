25144e6740c901fb975e6f5bae7ecd960a1fb7bb
Zsolt Makrai <mecrai@gmail.com>
2017-06-13 19:29:00 +0200

    server test

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/info', (req, res) => {
-    res.send(process.env);
+    res.send(process.env.APP_ENV);
 });
 
 if (process.env.APP_ENV === 'MOCK') {
