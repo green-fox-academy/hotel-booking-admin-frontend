3104a57e9131dede8df68c032800f46bd619df50
Zsolt Makrai <mecrai@gmail.com>
2017-06-14 00:14:57 +0200

    feat(style): main css file to assets folder, linked to the sub-pages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home-page',
-    templateUrl: './home.component.html'
-    // styleUrls: ['./app.component.css']
+    templateUrl: './home.component.html',
+    styleUrls: ['../assets/app.component.css']
 })
 export class HomeComponent {
     title = 'Home'
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'login-page',
-    templateUrl: './login.component.html'
-    // styleUrls: ['./app.component.css']
+    templateUrl: './login.component.html',
+    styleUrls: ['../assets/app.component.css']
 })
 export class LoginComponent {
     title = 'Login'
