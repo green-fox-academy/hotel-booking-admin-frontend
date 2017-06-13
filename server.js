86c8856758d573889797a2b8f57a1d2badfc44b9
Zsolt Makrai <mecrai@gmail.com>
2017-06-13 22:11:40 +0200

    feat(server): force ssl added

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const path = require('path');
 
 const app = express();
 
+const forceSSL = function() {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+};
+
+app.use(forceSSL());
+
 app.use(express.static(__dirname + '/dist'));
 
 if (process.env.APP_ENV === 'MOCK') {
