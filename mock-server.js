6075c76dd28e7957100d2eae2163067238450a5a
Zsolt Makrai <mecrai@gmail.com>
2017-06-13 17:28:22 +0200

    feat(mock-server): server file created

--- mock-server.js
+++ mock-server.js
@@ -1,36 +1,31 @@
 'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const MockServer = function () {
+    const user = {
+        username: 'Test Admin',
+        password: '1234'
+    };
+    const validResponse = {
+        status: 'ok',
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3RBZG1pbiIsImFkbWluIjp0cnVlfQ.nhC1EDI5xLGM4yZL2VMZyvHcbcWiXM2RVS7Y8Pt0Zuk'
+    };
 
-const app = express();
+    const invalidResponse = {
+        status: 'error',
+        message: 'Mismatched username and password'
+    };
 
-app.use(express.static(__dirname + '/dist'));
+    app.use(bodyParser.json());
 
-const user = {
-    username: 'Test Admin',
-    password: '1234'
+    app.post('/api/login/', (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        if (username === user.username && password === user.password) {
+            res.send(validResponse);
+        } else {
+            res.send(invalidResponse);
+        }
+    });
 };
-const validResponse = {
-    status: 'ok',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3RBZG1pbiIsImFkbWluIjp0cnVlfQ.nhC1EDI5xLGM4yZL2VMZyvHcbcWiXM2RVS7Y8Pt0Zuk'
-};
 
-const invalidResponse = {
-    status: 'error',
-    message: 'Mismatched username and password'
-};
-
-app.use(bodyParser.json());
-
-app.post('/api/login/', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    if (username === user.username && password === user.password) {
-        res.send(validResponse);
-    } else {
-        res.send(invalidResponse);
-    }
-});
-
-app.listen(process.env.PORT || 3000, () => console.log('server is running'));
+module.exports = MockServer;
---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mockServer = require('./mock-server.js');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+if (process.env.APP_ENV === 'MOCK') {
+    mockServer();
+} else {
+
+}
+
+app.listen(process.env.PORT || 3000, () => console.log('server is running'));
