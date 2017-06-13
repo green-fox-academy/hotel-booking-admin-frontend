6c9f14fd1649721a0db383f900a4bdbf75db9176
Zsolt Makrai <mecrai@gmail.com>
2017-06-13 14:28:33 +0200

    feat(mock server): server is up and running with post request

---
+++ mock-server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const user = {
+    username: 'Test Admin',
+    password: '1234'
+};
+const validResponse = {
+    status: 'ok',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3RBZG1pbiIsImFkbWluIjp0cnVlfQ.nhC1EDI5xLGM4yZL2VMZyvHcbcWiXM2RVS7Y8Pt0Zuk'
+};
+
+const invalidResponse = {
+    status: 'error',
+    message: 'Mismatched username and password'
+};
+
+app.use(bodyParser.json());
+
+app.post('/api/login/', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username === user.username && password === user.password) {
+        res.send(validResponse);
+    } else {
+        res.send(invalidResponse);
+    }
+});
+
+app.listen(3000, () => console.log('server is running'));
