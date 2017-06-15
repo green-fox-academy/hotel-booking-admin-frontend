89e8cd50439bcb0fa57aef39d4a24ab5b2e642fc
Balint Dorner <dorner.balint@gmail.com>
2017-06-15 16:02:32 +0200

    feat(login): login request files added

---
+++ src/app/loginservice.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+//import { Comment } from '..';
+import { Observable } from 'rxjs/Rx';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+
+@Injectable()
+export class LoginService {
+
+  constructor(private http: Http) { }
+
+
+  postLoginDetails(loginDetails): Observable<Comment[]> {
+    let endpoint = 'http://localhost:8080/api/login';
+ //   let bodyString = JSON.stringify(body);
+    let bodyString = loginDetails;
+    let headers = new Headers({ 'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
+
+    return this.http.post(endpoint, bodyString, options)
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'server error'));
+  }
+}
