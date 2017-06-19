8ac1308a76a914625615cf493203a604a01c6b05
Zsolt Makrai <mecrai@gmail.com>
2017-06-19 18:19:09 +0200

    refactor(home): home-page is commented out in index.html temporarly

--- src/app/login/loginservice.service.spec.ts
+++ src/app/login/loginservice.service.spec.ts
@@ -1,15 +1,29 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule, Http, BaseRequestOptions, XHRBackend } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 import { LoginService } from './loginservice.service';
 
 describe('LoginService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [LoginService]
+            imports: [HttpModule],
+            providers: [
+                LoginService,
+                { provide: XHRBackend, useClass: MockBackend }
+            ]
         });
     });
 
     it('should be created', inject([LoginService], (service: LoginService) => {
-    expect(service).toBeTruthy();
+        expect(service).toBeTruthy();
     }));
+
+    describe('postLoginDetails()', () => {
+
+        it('should return an Observable<Comment[]>', inject([/* provider tokens */], (/* dependencies */) => {
+
+        }));
+
+    });
 });
