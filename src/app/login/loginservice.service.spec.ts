1a8a74a1b5e8f2202ed5f63b7a85e039cf07bae3
Zsolt Makrai <mecrai@gmail.com>
2017-06-20 13:20:07 +0200

    test(http): still not working

--- src/app/login/loginservice.service.spec.ts
+++ src/app/login/loginservice.service.spec.ts
@@ -1,24 +1,26 @@
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import {
     HttpModule,
-    BaseRequestOptions,
+    Http,
     XHRBackend,
+    ResponseOptions,
     Response,
-    Headers,
-    RequestOptions
+    Headers
 } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 
 import { LoginService } from './loginservice.service';
 
 describe('LoginService', () => {
+
     beforeEach(() => {
+
         TestBed.configureTestingModule({
             imports: [HttpModule],
             providers: [
-                { useValue: 'http://localhost:8080/api/login/' }
+                { useValue: 'http://localhost:8080/api/login/' },
                 LoginService,
-                { provide: XHRBackend, useClass: MockBackend }
+                { provide: XHRBackend, useClass: MockBackend },
             ]
         });
     });
@@ -28,11 +30,11 @@ describe('LoginService', () => {
     }));
 
     describe('postLoginDetails()', () => {
-        it('should return an Observable<Comment[]>', inject([LoginService, XHRBackend], (loginService, mockBackend) => {
+        it('should return an Observable<Comment[]>', inject([LoginService, XHRBackend], (LoginService, mockBackend) => {
             const mockResponse = {
                     status: 'ok',
                     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlRlc3RBZG1pbiIsImFkbWluIjp0cnVlfQ.nhC1EDI5xLGM4yZL2VMZyvHcbcWiXM2RVS7Y8Pt0Zuk'
-            }
+            };
 
         mockBackend.connections.subscribe((connection) => {
             connection.mockRespond(new Response(new ResponseOptions({
@@ -48,8 +50,8 @@ describe('LoginService', () => {
             LoginService.postLoginDetails(loginDetails).subscribe((userInfo) => {
                 expect(userInfo.length).toBe(2);
                 expect(userInfo.status).toEqual('ok');
-            }
-        })));
+            });
 
+        }));
     });
 });
