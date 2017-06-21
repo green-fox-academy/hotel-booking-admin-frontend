9327c1dd3a908f46a821688531ae39535d689207
Zsolt Makrai <mecrai@gmail.com>
2017-06-21 11:37:20 +0200

    feat(register): files created

---
+++ src/app/register/register.component.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { LoginComponent } from './login.component';
+import { LoginService } from './loginservice.service';
+
+describe('LoginComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+        imports: [
+            RouterTestingModule,
+            FormsModule,
+            HttpModule
+        ],
+        declarations: [
+            LoginComponent
+        ],
+        providers: [LoginService]
+    }).compileComponents();
+  }));
+
+  it('should create the login', async(() => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const login = fixture.debugElement.componentInstance;
+    expect(login).toBeTruthy();
+  }));
+
+  it(`should have as title 'Login'`, async(() => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const login = fixture.debugElement.componentInstance;
+    expect(login.title).toEqual('Login');
+  }));
+
+  it('should render title in a h2 tag', async(() => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Login');
+  }));
+});
