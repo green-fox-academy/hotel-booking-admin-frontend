ca3b3850899e5cd50a0df0895383b7efe374b466
Balint Dorner <dorner.balint@gmail.com>
2017-06-21 13:44:05 +0200

    feat(regHotel): res contains real data now

--- mock-decorator.js
+++ mock-decorator.js
@@ -13,29 +13,6 @@ const MockServer = function(app) {
         password: string,
     }
 
-    let hotel = {
-        links: {
-            self: 'https://your-hostname.com/hotels/1'
-        },
-        data: {
-            type: 'hotels',
-            id: '1',
-            attributes: {
-                location: 'Budapest',
-                name: 'Hotel Ipoly utca',
-                has_wifi: true,
-                has_parking: true,
-                has_pets: true,
-                has_restaurant: true,
-                has_bar: true,
-                has_swimming_pool: true,
-                has_air_conditioning: true,
-                has_gym: true,
-                meal_plan: 'american-plan',
-                stars: 5
-            }
-        }
-    }
 
     const validResponse = {
         data: {
@@ -66,6 +43,30 @@ const MockServer = function(app) {
         }
     }
 
+    let hotelResponse = {
+        links: {
+            self: 'https://your-hostname.com/hotels/1'
+        },
+        data: {
+            type: 'hotels',
+            id: '1',
+            attributes: {
+                location: 'Budapest',
+                name: 'Hotel Ipoly utca',
+                has_wifi: true,
+                has_parking: true,
+                has_pets: true,
+                has_restaurant: true,
+                has_bar: true,
+                has_swimming_pool: true,
+                has_air_conditioning: true,
+                has_gym: true,
+                meal_plan: 'american-plan',
+                stars: 5
+            }
+        }
+    }
+
     if (process.env.APP_ENV === 'MOCK') {
         app.get('/*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
     } else {
@@ -83,7 +84,7 @@ const MockServer = function(app) {
     });
 
     app.post('/api/register/', (req, res) => {
-        regResponse.data.type = req.body.data.type
+        regResponse.data.type = req.body.data.type;
         regResponse.data.attributes.email = req.body.data.attributes.email;
 
         user2.email = req.body.data.attributes.email;
@@ -93,13 +94,21 @@ const MockServer = function(app) {
     });
 
     app.post('/hotel/', (req, res) => {
-        regResponse.data.type = req.body.data.type
-        regResponse.data.attributes.email = req.body.data.attributes.email;
+        hotelResponse.data.type = req.body.data.type;
+        hotelResponse.data.attributes.location = req.body.data.attributes.location;
+        hotelResponse.data.attributes.name = req.body.data.attributes.name;
+        hotelResponse.data.attributes.has_wifi = req.body.data.attributes.has_wifi;
+        hotelResponse.data.attributes.has_parking = req.body.data.attributes.has_parking;
+        hotelResponse.data.attributes.has_pets = req.body.data.attribute.has_pets;
+        hotelResponse.data.attributes.has_restaurant = req.body.data.attributes.has_restaurant;
+        hotelResponse.data.attributes.has_bar = req.body.data.attributes.has_bar;
+        hotelResponse.data.attributes.has_swimming_pool = req.body.data.attributes.has_swimming_pool;
+        hotelResponse.data.attributes.has_air_conditioning = req.body.data.attributes.has_air_conditioning;
+        hotelResponse.data.attributes.has_gym = req.body.data.attributes.has_gym;
+        hotelResponse.data.attributes.meal_plan = req.body.data.attributes.meal_plan;
+        hotelResponse.data.attributes.stars = req.body.data.attributes.stars;
 
-        user2.email = req.body.data.attributes.email;
-        user2.password = req.body.data.attributes.password;
-
-        res.send(regResponse);
+        res.status(201).send(hotelResponse);
     });
 }
 
