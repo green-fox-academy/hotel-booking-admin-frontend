a47911f467d2c5b2b4b46b8f1525c8f3b8d0f86e
Fanni Fulmer <fannifulmer@gmail.com>
2017-06-21 11:29:31 +0200

    feat(hotelregistration): http post request created

---
+++ src/app/hotelregistration/hotelregistration.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HotelregistrationService } from './hotelregistration.service';
+import { Router } from '@angular/router';
+import { Hotel } from './hotels'
+
+@Component({
+    selector: 'addhotel-page',
+    templateUrl: './hotelregistration.component.html',
+    styleUrls: ['../assets/app.component.scss'],
+    providers: [HotelregistrationService]
+})
+
+export class LoginComponent {
+    hotel = new Hotel;
+    constructor (
+        private hotelregistrationservice: HotelregistrationService,
+        public router: Router) { }
+
+
+    onRegistration() {
+        this.hotelregistrationservice.addHotels(this.hotel)
+            .subscribe(
+                response => {
+                    this.hotel.attributes
+                },
+                error => alert(error),
+                () => {
+                }
+            );
+    }
+}
---
+++ src/app/hotelregistration/hotels.ts
@@ -0,0 +1,19 @@
+export class Hotel {
+    data: {
+         type: string,
+    }
+    attributes: {
+        location: string,
+        name: string,
+        has_wifi: boolean,
+        has_parking: boolean,
+        has_pets: boolean,
+        has_restaurant: boolean,
+        has_bar: boolean,
+        has_swimming_pool: boolean,
+        has_air_conditioning: boolean,
+        has_gym: boolean,
+        meal_plan: string,
+        stars: number
+    }
+}
