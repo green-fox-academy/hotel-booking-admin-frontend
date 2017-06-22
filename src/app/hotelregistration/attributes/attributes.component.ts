f9e4b5c681139bf749a64800bc7daac67650d134
Balint Dorner <dorner.balint@gmail.com>
2017-06-22 15:10:45 +0200

    refactor(hotel): hotel service added and attribute component created

--- src/app/hotelregistration/attributes/attributes.component.ts
+++ src/app/hotelregistration/attributes/attributes.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HotelService } from '../hotel.service';
+import { HotelComponent } from '../hotelregistration.component'
+
 @Component({
   selector: 'hotel-attributes',
   templateUrl: './attributes.component.html',
-  styleUrls: ['./attributes.component.scss']
+  styleUrls: ['../../assets/app.component.scss']
 })
 export class AttributesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public hotelservice: HotelService,
+  ) {  }
 
   ngOnInit() {
   }
-
 }
--- src/app/hotelregistration/hotelregistration.component.ts
+++ src/app/hotelregistration/hotelregistration.component.ts
@@ -2,32 +2,30 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { HotelregistrationService } from './hotelregistration.service';
 import { Router } from '@angular/router';
-import { Hotel } from './hotels';
+import { HotelService } from './hotel.service';
 import { AttributesComponent } from './attributes/attributes.component';
 
 @Component({
     selector: 'addhotel-page',
     templateUrl: './hotelregistration.component.html',
     styleUrls: ['../assets/app.component.scss'],
-    providers: [HotelregistrationService, AttributesComponent]
+    providers: [HotelregistrationService, HotelService]
     
 })
 
 export class HotelComponent {
     title = 'Add Hotel';
-    hotel = new Hotel;
     loading = false;
     hotelDetails;
 
     constructor (
         private hotelregistrationservice: HotelregistrationService,
-        public router: Router) {     
-        }
-
+        public hotelservice: HotelService,
+        public router: Router) {  }
 
     onRegistration() {
         this.loading = true;
-        this.hotelregistrationservice.addHotels(this.hotel)
+        this.hotelregistrationservice.addHotels(this.hotelservice.hotel)
             .subscribe(
                 response => {
                     // this.hotelDetails = response;
