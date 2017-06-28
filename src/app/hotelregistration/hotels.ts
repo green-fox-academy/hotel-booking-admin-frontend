8668cce4d4e288ca0cdcf310c9a06a9e465a4852
Balint Dorner <dorner.balint@gmail.com>
2017-06-28 14:05:02 +0200

    feat(hotels): hotel attributes updated with new parameters

--- src/app/hotelregistration/hotels.ts
+++ src/app/hotelregistration/hotels.ts
@@ -1,9 +1,11 @@
 export class Hotel {
     data = {
         type: 'hotels',
+        id: '',
         attributes: {
             location: '',
             name: '',
+            main_image_src: '',
             has_wifi: '',
             has_parking: '',
             has_pets: '',
@@ -13,6 +15,11 @@ export class Hotel {
             has_air_conditioning: '',
             has_gym: '',
             meal_plan: '',
+            user_id: '',
+            booking_id: '',
+            amount: '',
+            currency: '',
+            status: '',
             stars: ''
         }
     }
