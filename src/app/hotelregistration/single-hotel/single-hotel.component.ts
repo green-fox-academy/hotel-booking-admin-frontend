e954da96af6a12964611277aef06c27f72a8dec8
Balint Dorner <dorner.balint@gmail.com>
2017-06-28 18:09:04 +0200

    feat(modify_hotel): seems working with small issues(no image received after modification)

--- src/app/hotelregistration/single-hotel/single-hotel.component.ts
+++ src/app/hotelregistration/single-hotel/single-hotel.component.ts
@@ -3,17 +3,33 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { HotelService } from '../hotel.service';
+import { HttpService } from '../../httprequest.service';
+
 
 @Component({
   selector: 'app-single-hotel',
   templateUrl: './single-hotel.component.html',
   styleUrls: ['../../assets/app.component.scss'],
+  providers: [HttpService],
 })
 export class SingleHotelComponent implements OnInit {
 
   constructor(
+    private updateservice: HttpService,
     public hotelservice: HotelService,
   ) { console.log(hotelservice.hotel.hotelwithID2)}
+
+  updateHotel() {
+        const endpoint = 'https://cake-cup.glitch.me/api/hotels/'+ this.hotelservice.hotel.hotelwithID2.data.id;
+        this.updateservice.httpRequest(this.hotelservice.hotel, endpoint, 'patch')
+            .subscribe(
+                response => {
+                    console.log(response)
+                },
+                error => {
+                    console.error(error)
+                });
+  }
 
   ngOnInit(
   ) { }
