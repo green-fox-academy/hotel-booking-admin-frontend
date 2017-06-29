41436ab375b838b118e26da371019d09cf839de2
Balint Dorner <dorner.balint@gmail.com>
2017-06-29 16:30:12 +0200

    refactor(http_request): uses .request method

--- src/app/httprequest.service.ts
+++ src/app/httprequest.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Http, Response, Headers, RequestOptions, Request, RequestMethod } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
 import 'rxjs/add/operator/map';
@@ -8,35 +8,17 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class HttpService {
 
-    constructor(private http: Http) { }
-
-    httpRequest(UserDetails, endpoint, method): Observable<Comment[]> {
-        const bodyString = UserDetails;
-        const headers = new Headers({ 'Content-Type': 'application/json'});
-        const options = new RequestOptions({headers: headers});
-        let request;
-
-        switch (method) {
-            case 'post': {
-                request = this.http.post(endpoint, bodyString, options);
-                break;
-            }
-            case 'get': {
-                request = this.http.get(endpoint, options);
-                break;
-            }
-            case 'patch': {
-                request = this.http.patch(endpoint, bodyString, options);
-                break;
-            }
-            case 'delete': {
-                request = this.http.delete(endpoint);
-                break;
-            }
-        };
-
-        return request
-            .map((res: Response) => res.json())
-            .catch((error: any) => Observable.throw(error.json() || 'server error'));
+    constructor( private http: Http ) { }
+    
+    httpRequest(bodies, endpoint, reqMethod): Observable<Comment[]> {
+        return this.http.request(new Request({
+            method: reqMethod,
+            url: endpoint,
+            body: bodies
+        }))
+        .map((res: Response) => res.json())
+        .catch((error: any) => Observable.throw(error.json() || 'server error'));
     }
 }
+
+
