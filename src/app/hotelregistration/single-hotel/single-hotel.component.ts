f39a86aee93f52954598a33f84910ccda5c19b19
Zsolt Makrai <mecrai@gmail.com>
2017-06-30 12:10:02 +0200

    feat(autosave): displays the message, but the timer is not working properly

--- src/app/hotelregistration/single-hotel/single-hotel.component.ts
+++ src/app/hotelregistration/single-hotel/single-hotel.component.ts
@@ -8,52 +8,57 @@ import { GetHotelsService } from '../get-hotels.service';
 
 
 @Component({
-  selector: 'app-single-hotel',
-  templateUrl: './single-hotel.component.html',
-  styleUrls: ['../../assets/app.component.scss'],
-  providers: [HttpService],
+    selector: 'app-single-hotel',
+    templateUrl: './single-hotel.component.html',
+    styleUrls: ['../../assets/app.component.scss'],
+    providers: [HttpService],
 })
 export class SingleHotelComponent implements OnInit {
-  timeoutId;
-  timeRestarter;
+    timeoutId;
+    timeRestarter;
+    autosaveVisible = true;
+    timeAgo = 0;
+    counter;
 
-  constructor(
-      private updateservice: HttpService,
-      public hotelservice: HotelService,
-      public gethotelservice: GetHotelsService,
-      public router: Router,
-  ) { }
-
-  updateHotel(id) {
-      const endpoint = 'api/hotels/'+ id;
-      console.log(this.hotelservice.hotel.hotelWithId.data)
-      this.updateservice.httpRequest(this.hotelservice.hotel.hotelWithId.data, endpoint, 'patch')
-          .subscribe(
-              response => {
-                  this.gethotelservice.getHotels()
-                  
-              },
-              error => {
-                  console.error(error)
-              }
-          );
-  }
-  
-  autoSave(id) {
-      this.timeRestarter = clearTimeout(this.timeoutId);
-      this.timeoutId = setTimeout(() => this.updateHotel(id), 1000);
-
-  }
+    constructor(
+        private updateservice: HttpService,
+        public hotelservice: HotelService,
+        public gethotelservice: GetHotelsService,
+        public router: Router,
+    ) { }
 
 
+    updateHotel(id) {
+        const endpoint = 'api/hotels/' + id;
+        this.autosaveVisible = false;
+        console.log(this.hotelservice.hotel.hotelWithId.data)
+        this.updateservice.httpRequest(this.hotelservice.hotel.hotelWithId.data, endpoint, 'patch')
+            .subscribe(
+                response => {
+                    this.gethotelservice.getHotels()
 
-  redirect() {
-    this.router.navigate(['hotels'])
-  }
+                },
+                error => {
+                    console.error(error)
+                }
+            );
+        }
 
+    setTimeAgo () {
+        this.timeAgo++
+    };
 
+    autoSave(id) {
+        this.timeAgo = 0;
+        this.timeRestarter = clearTimeout(this.timeoutId);
+        this.timeoutId = setTimeout(() => this.updateHotel(id), 1000);
+        this.counter = setInterval(this.setTimeAgo, 1000);
+    }
 
-  ngOnInit(
-  ) { }
+    redirect() {
+        this.router.navigate(['hotels'])
+    }
 
+    ngOnInit(
+    ) { }
 }
