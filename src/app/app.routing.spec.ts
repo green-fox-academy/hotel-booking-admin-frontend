031dc861a87c7201146ef0145a81e93ed13bc3c1
Fanni Fulmer <fannifulmer@gmail.com>
2017-07-03 13:50:02 +0200

    test(routing): fixed router test

--- src/app/app.routing.spec.ts
+++ src/app/app.routing.spec.ts
@@ -6,13 +6,13 @@ import { async, inject, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { Http, ConnectionBackend, RequestOptions } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import {
-//     HttpModule,
-//     XHRBackend,
-//     ResponseOptions,
-//     Response
-// } from '@angular/http';
-// import { MockBackend, MockConnection } from '@angular/http/testing';
+import {
+    HttpModule,
+    XHRBackend,
+    ResponseOptions,
+    Response
+} from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -27,8 +27,6 @@ import { StarratingComponent } from './hotelregistration/starrating/starrating.c
 import { GetHotelsService } from './hotelregistration/get-hotels.service'
 import { HotelService } from './hotelregistration/hotel.service'
 import { HttpService } from './httprequest.service';
-import { GetHotelsService } from './hotelregistration/get-hotels.service';
-import { HotelService } from './hotelregistration/hotel.service';
 
 class RequestOption {
     public requestoption: RequestOptions
@@ -50,7 +48,7 @@ describe('RoutingComponent', () => {
         TestBed.configureTestingModule({
             imports: [
                 FormsModule,
-                // HttpModule,
+                HttpModule,
                 RouterTestingModule.withRoutes([
                     { path: 'login', component: LoginComponent },
                     { path: 'register', component: RegisterComponent },
@@ -79,14 +77,9 @@ describe('RoutingComponent', () => {
                 ConnectionBackend,
                 GetHotelsService,
                 HttpService,
-<<<<<<< HEAD
                 { provide: XHRBackend, useClass: MockBackend },
                 GetHotelsService,
                 HotelService
-=======
-                HotelService
-                // { provide: XHRBackend, useClass: MockBackend },
->>>>>>> d2b717b39439e6694fe8ff25168eaf7d61c279b8
             ]
         });
     });
