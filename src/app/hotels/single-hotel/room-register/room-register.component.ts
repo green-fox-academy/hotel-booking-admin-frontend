a6ab9c816d5d372a3d2e4e6f9ae3946542a6a4a5
Zsolt Makrai <mecrai@gmail.com>
2017-07-05 21:12:15 +0200

    refactor(room reg): texarea changed, added more currencies, ngclass fro spinner

--- src/app/hotels/single-hotel/room-register/room-register.component.ts
+++ src/app/hotels/single-hotel/room-register/room-register.component.ts
@@ -9,16 +9,20 @@ import { HttpService } from '../../../httprequest.service';
   providers: [HttpService]
 })
 export class RoomRegisterComponent implements OnInit {
+    loading = false;
 
   constructor(
-    public hotelservice: HotelService, 
-    private updateservice: HttpService) { }
+    public hotelservice: HotelService,
+    private updateservice: HttpService
+    ) { }
 
     roomlister(id) {
+        this.loading = true;
         const endpoint = 'api/hotels/' + id + 'relationships/rooms';
         this.updateservice.httpRequest('', endpoint, 'get')
             .subscribe(
                 response => {
+                    this.loading = false;
                 },
                 error => {
                     console.error(error)
