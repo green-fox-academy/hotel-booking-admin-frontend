dd42f796314e59edf021eaa0b11ad686ab72f499
Fanni Fulmer <fannifulmer@gmail.com>
2017-07-05 16:28:58 +0200

    feat(rooms): started creating get request

--- src/app/hotels/single-hotel/room-register/room-register.component.ts
+++ src/app/hotels/single-hotel/room-register/room-register.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { HotelService } from '../../hotel.service';
+import { HttpService } from '../../../httprequest.service';
 
 @Component({
   selector: 'app-room-register',
   templateUrl: './room-register.component.html',
-  styleUrls: ['../../../../app/assets/app.component.scss']
+  styleUrls: ['../../../../app/assets/app.component.scss'],
+  providers: [HttpService]
 })
 export class RoomRegisterComponent implements OnInit {
 
-  constructor(public hotelservice: HotelService) { }
+  constructor(
+    public hotelservice: HotelService, 
+    private updateservice: HttpService) { }
+
+    roomlister(id) {
+        const endpoint = 'api/hotels/' + id + 'relationships/rooms';
+        this.updateservice.httpRequest('', endpoint, 'get')
+            .subscribe(
+                response => {
+                },
+                error => {
+                    console.error(error)
+                }
+            );
+        }
 
   ngOnInit() {
   }
---
+++ src/app/hotels/single-hotel/room-register/rooms.ts
@@ -0,0 +1,13 @@
+export class Room {
+    data = {
+        type: 'rooms',
+        id: '1',
+          attributes: {
+            price: '12',
+            currency: 'EUR',
+            name: '',
+            description: '',
+            max_occupancy: '20'
+          }
+        }
+    };
