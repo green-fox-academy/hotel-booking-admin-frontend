10d82e4e1aa91b98d89ff55347b2c8943eb2b3c3
Fanni Fulmer <fannifulmer@gmail.com>
2017-07-05 11:31:13 +0200

    refactor(room): imported hotelservice, now it lists input fields correctly

--- src/app/hotels/single-hotel/room-register/room-register.component.ts
+++ src/app/hotels/single-hotel/room-register/room-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HotelService } from '../../hotel.service';
 
 @Component({
   selector: 'app-room-register',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomRegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public hotelservice: HotelService) { }
 
   ngOnInit() {
   }
