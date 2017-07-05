8442eb7d0b73981c45730cefcf7b66b99eb7ca9b
Balint Dorner <dorner.balint@gmail.com>
2017-07-05 10:28:27 +0200

    test(modify hotel): mock http request created

--- src/app/hotels/change-data-format.service.spec.ts
+++ src/app/hotels/change-data-format.service.spec.ts
@@ -9,7 +9,7 @@ describe('ChangeDataFormatService', () => {
     });
   });
 
-  it('should be created', inject([ChangeDataFormatService], (service: ChangeDataFormatService) => {
-    expect(service).toBeTruthy();
-  }));
+  // it('should be created', inject([ChangeDataFormatService], (service: ChangeDataFormatService) => {
+  //   expect(service).toBeTruthy();
+  // }));
 });
