1f3edde669c5a2d303402fb483cd5bd9d312fbd4
Zsolt Makrai <mecrai@gmail.com>
2017-07-12 11:15:19 +0200

    test(rooms): import components

--- src/app/hotels/single-hotel/rooms/rooms.component.spec.ts
+++ src/app/hotels/single-hotel/rooms/rooms.component.spec.ts
@@ -1,25 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 
 import { RoomsComponent } from './rooms.component';
+import { RoomService } from '../room-register/room-service';
+import { HotelService } from '../../hotel.service';
 
 describe('RoomsComponent', () => {
-  let component: RoomsComponent;
-  let fixture: ComponentFixture<RoomsComponent>;
+    let component: RoomsComponent;
+    let fixture: ComponentFixture<RoomsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RoomsComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ HttpModule ],
+            declarations: [ RoomsComponent ],
+            providers: [
+                RoomService,
+                HotelService
+            ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RoomsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RoomsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
 });
