1e66d104296c7967c36bad43bdfaf5de321ba9c7
Fanni Fulmer <fannifulmer@gmail.com>
2017-07-12 13:46:33 +0200

    refactor(getroomsservice): created new service for getting rooms

--- src/app/hotels/single-hotel/room-register/room-service.ts
+++ src/app/hotels/single-hotel/room-register/room-service.ts
@@ -1,25 +1,9 @@
 import { Injectable } from '@angular/core';
 
 import { Room } from './rooms'
-import { HttpService } from '../../../httprequest.service';
 
 @Injectable()
 export class RoomService {
-    constructor(
-    public httpservice: HttpService
-    ) { }
-
     room = new Room;
 
-    getRooms(hotelId) {
-    const endpoint = 'api/hotels/' + hotelId + '/relationships/rooms';
-    this.httpservice.httpRequest('', endpoint, 'get')
-        .subscribe(
-            response => {
-                this.room.roomList = response
-                console.log(this.room.roomList)
-            },
-            error => console.error(error)
-        );
-    }
 }
