7637e5740e5f9a3e84521c9d959568b21c8abb11
Fanni Fulmer <fannifulmer@gmail.com>
2017-07-14 10:42:51 +0200

    feat(drag-ndrop service): created drag and drop service

--- src/app/hotels/drag-n-drop/drag-n-drop.component.ts
+++ src/app/hotels/drag-n-drop/drag-n-drop.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['../../assets/app.component.scss']
 })
 export class DragNDropComponent implements OnInit {
+    // currentStatus: number;
+    // uploadFieldName = 'photos';
+
+    // readonly STATUS_INITIAL = 0;
+    // readonly STATUS_SAVING = 1;
+    // readonly STATUS_SUCCESS = 2;
+    // readonly STATUS_FAILED = 3;
     private fileList: any = [];
     private invalidFiles: any = [];
 
